9a6ced4c55a71b7dc727278820cd518af9b9d07e
herbchow <HerbertC@iqmetrix.com>
2012-11-20 14:57:41 -0800

    Pushing new changes

---
+++ source/prep/collections/HerbExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using prep.utility.filtering;
+
+namespace prep.collections
+{
+    public static class MovieListExtensions
+    {
+        public static IProvideAccessToFilteringExtensions<ItemToFilter,TProperty> where<ItemToFilter,TProperty>(this IEnumerable<ItemToFilter> self,PropertyAccessor<ItemToFilter, TProperty> accessor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

ecd39e3570bd7acd069200d167042d87dfc1efc4
herbchow <HerbertC@iqmetrix.com>
2012-11-20 10:41:13 -0800

    Pushing new changes

---
+++ source/prep.specs/gemskata/GemsSpecs.cs
@@ -0,0 +1,73 @@
+﻿using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+
+namespace prep.gemskata
+{
+    public class GemsSpecs
+    {
+        [Subject(typeof(GemsSolver))]
+        public abstract class gems_solver_concern : Observes<GemsSolver>
+        {
+            protected static string input =
+                "5 " +
+"1 2" +
+"2 2" +
+"3 4" +
+"4 3" +
+"5 5";
+
+            protected static GemsSolver Solver;
+            Establish c = () =>
+                              {
+                              };
+        };
+
+        public class when_input_is_1_2 : gems_solver_concern
+        {
+            static int result;
+
+            Establish c = () =>
+                Solver = new GemsSolver(1, 2);
+            Because b = () =>
+                result = Solver.Solve();
+
+            It should_return_7 = () =>
+                                     {
+                                         result.ShouldEqual(7);
+
+                                     };
+        }
+
+        public class when_input_is_2_2 : gems_solver_concern
+        {
+            static int result;
+
+            Establish c = () =>
+                Solver = new GemsSolver(2, 2);
+            Because b = () =>
+                result = Solver.Solve();
+
+            It should_return_71 = () =>
+            {
+                result.ShouldEqual(71);
+
+            };
+        }
+
+        public class when_input_is_3_4 : gems_solver_concern
+        {
+            static int result;
+
+            Establish c = () =>
+                Solver = new GemsSolver(3, 4);
+            Because b = () =>
+                result = Solver.Solve();
+
+            It should_return_211351945 = () =>
+            {
+                result.ShouldEqual(211351945);
+
+            };
+        }
+    }
+}
---
+++ source/prep/gemskata/GemsSolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prep.gemskata
+{
+    public class GemsSolver
+    {
+        public GemsSolver(int n,int k)
+        {
+            
+        }
+
+        public int Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
